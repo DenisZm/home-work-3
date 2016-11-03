99fb694e518e4448101e71549f1b451e7adf3500
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2016-11-03 19:37:45 +0200

    Second commit

--- src/Array1.java
+++ src/Array1.java
@@ -4,16 +4,17 @@
  *
  * Created by Denis on 25.10.2016.
  */
+
 public class Array1 {
     public static void main(String[] args) {
-        int n = Helper.inputInt("Input array size:");
+        int n = (int) Helper.inputDouble("Input array size:");
         int[] arr = Helper.createRandomArray(n);
 
         System.out.println("Array: ");
         Helper.printArray(arr);
         System.out.println();
 
-        System.out.println("Minimal:");
+        System.out.println("Minimal in array:");
         System.out.printf("%4d", Helper.findMinInArray(arr));
     }
 }
