ac124073e7c8a4d0d4fb74e19ae146480251f321
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2016-11-03 22:13:32 +0200

    Change output format in RightTriangleArea

--- src/RightTriangleArea.java
+++ src/RightTriangleArea.java
@@ -9,7 +9,7 @@ public class RightTriangleArea {
         double a = Helper.inputDouble("Input A side:");
         double b = Helper.inputDouble("Input B side:");
         System.out.println();
-        System.out.printf("Area of triangle is: %s%n", area(a, b));
+        System.out.printf("Area of triangle is: %.3f%n", area(a, b));
     }
 
     private static double area(double a, double b) {
